4a454fec75da4c204d2c2698dba390249e9ba123
wxl4489627487 <Administrator@20160520-115823>
2017-02-14 15:24:31 +0800

    111111111111111111111112

--- pages/filter/filter.js
+++ pages/filter/filter.js
@@ -1,7 +1,4 @@
 // pages/filter/filter.js
-
-var tcity = require("../../utils/city.js");
-
 var app = getApp()
 var remoteAddress = app.remoteAddressdxf();
 var openId = app.getSysOpenId();
@@ -26,10 +23,7 @@ Page({
         disabled: !this.data.disabled
       })
     },
-
     businessCategoryList: '',
-    locationArray: ['河北', '河南', '安徽', '云南', '贵州', '江西', '广州', '广西', '福建', '山东'],
-    locationArray2: ['信阳市', '保定市', '合肥市', '南阳市'],
     typeFlag: "",
     childCategoryList: '',
     chidCatIndex: 0,
@@ -45,100 +39,6 @@ Page({
     hides: false,
     hide: true,
     wxSData: '',
-    locationArray3: ['清华大学', '北京大学', '安徽大学', '暨南大学'],
-    items: [
-      { value: '一级分类' },
-      { value: '二级分类', checked: 'true' },
-      { value: '二三级分类' },
-      { value: '分类' },
-      { value: '英国' },
-      { value: '法国' },
-    ],
-  },
-  locationIndex: 2,
-  locationIndex2: 1,
-  locationIndex2: 1,
-  chooseSchool(e) {
-    if (this.data.locationIndex3 || e.detail.value) {
-
-      this.setData({
-        locationIndex3: e.detail.value
-      });
-    }
-  },
-  handleLocationPickerChange(e) {
-    if (this.data.locationIndex || e.detail.value) {
-
-      this.setData({
-        locationIndex: e.detail.value
-      });
-    }
-  },
-  handleLocationPickerChange2(e) {
-    if (this.data.locationIndex2 || e.detail.value) {
-
-      this.setData({
-        locationIndex2: e.detail.value
-      });
-    }
-  },
-  bindChange: function (e) {
-    //console.log(e);
-    var val = e.detail.value
-    var t = this.data.values;
-    var cityData = this.data.cityData;
-
-    if (val[0] != t[0]) {
-      console.log('province no ');
-      const citys = [];
-      const countys = [];
-
-      for (let i = 0; i < cityData[val[0]].sub.length; i++) {
-        citys.push(cityData[val[0]].sub[i].name)
-      }
-      for (let i = 0; i < cityData[val[0]].sub[0].sub.length; i++) {
-        countys.push(cityData[val[0]].sub[0].sub[i].name)
-      }
-
-      this.setData({
-        province: this.data.provinces[val[0]],
-        city: cityData[val[0]].sub[0].name,
-        citys: citys,
-        county: cityData[val[0]].sub[0].sub[0].name,
-        countys: countys,
-        values: val,
-        value: [val[0], 0, 0]
-      })
-
-      return;
-    }
-    if (val[1] != t[1]) {
-      console.log('city no');
-      const countys = [];
-
-      for (let i = 0; i < cityData[val[0]].sub[val[1]].sub.length; i++) {
-        countys.push(cityData[val[0]].sub[val[1]].sub[i].name)
-      }
-
-      this.setData({
-        city: this.data.citys[val[1]],
-        county: cityData[val[0]].sub[val[1]].sub[0].name,
-        countys: countys,
-        values: val,
-        value: [val[0], val[1], 0]
-      })
-      return;
-    }
-    if (val[2] != t[2]) {
-      console.log('county no');
-      this.setData({
-        county: this.data.countys[val[2]],
-        values: val
-      })
-      return;
-    }
-
-
   },
   open: function () {
     this.setData({
@@ -153,12 +53,6 @@ Page({
     that.setData({
       typeFlag: e.typeFlag
     })
-    tcity.init(that);
-
-
-
-
-
 
     //一级分类
     wx.request({
@@ -215,35 +109,6 @@ Page({
         bussiLength = res.data.data.businessCategoryList.length;
       }
     })
-
-    var cityData = that.data.cityData;
-
-
-    const provinces = [];
-    const citys = [];
-    const countys = [];
-
-    for (let i = 0; i < cityData.length; i++) {
-      provinces.push(cityData[i].name);
-    }
-    console.log('省份完成');
-    for (let i = 0; i < cityData[0].sub.length; i++) {
-      citys.push(cityData[0].sub[i].name)
-    }
-    console.log('city完成');
-    for (let i = 0; i < cityData[0].sub[0].sub.length; i++) {
-      countys.push(cityData[0].sub[0].sub[i].name)
-    }
-
-    that.setData({
-      'provinces': provinces,
-      'citys': citys,
-      'countys': countys,
-      'province': cityData[0].name,
-      'city': cityData[0].sub[0].name,
-      'county': cityData[0].sub[0].sub[0].name
-    })
-    console.log('初始化完成');
 
   },
 
@@ -400,6 +265,13 @@ Page({
           spaceName = '',
           cbxgroupArr = ''
       }
+    })
+  },
+   resetHide: function () {
+    var that=this;
+    that.setData({
+      hides: false,
+      hide: true,
     })
   },
   onReady: function () {
